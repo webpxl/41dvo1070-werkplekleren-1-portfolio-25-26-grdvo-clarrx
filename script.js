d450a9cc5eb401e6fb3e8760222588a44577479f
larrx <12400776@student.pxl.be>
2026-01-04 22:11:33 +0100

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// Script klaar voor animaties of interactie
 console.log("Portfolio volledig geladen");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popup = document.getElementById("welcome-popup");
+    const closeBtn = document.getElementById("close-popup");
+
+    closeBtn.addEventListener("click", () => {
+        popup.style.display = "none";
+    });
+});
