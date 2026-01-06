2c50c50e7ae235852ba7bd95ea359b0742f7701b
larrx <12400776@student.pxl.be>
2026-01-06 15:35:09 +0100

    second commit

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ document.addEventListener("DOMContentLoaded", () => {
         popup.style.display = "none";
     });
 });
+
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+});
+
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
