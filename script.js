6106cd8b45b45637a68da0d75d6710c714eac167
larrx <12400776@student.pxl.be>
2026-01-07 19:42:40 +0100

    second commit

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-console.log("Portfolio volledig geladen");
-
 document.addEventListener("DOMContentLoaded", () => {
-    const popup = document.getElementById("welcome-popup");
-    const closeBtn = document.getElementById("close-popup");
-
-    closeBtn.addEventListener("click", () => {
-        popup.style.display = "none";
-    });
+    document.getElementById("close-popup").onclick = () =>
+        document.getElementById("welcome-popup").style.display = "none";
 });
 
-const backToTopBtn = document.getElementById("backToTop");
+const btn = document.getElementById("backToTop");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
+    btn.style.display = window.scrollY > 300 ? "block" : "none";
 });
 
-backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
+btn.onclick = () => window.scrollTo({top:0, behavior:"smooth"});
